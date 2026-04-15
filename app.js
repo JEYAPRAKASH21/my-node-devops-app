239d92d461671a69e7459270f0b65987e003cba9
JEYAPRAKASH21 <167057989+JEYAPRAKASH21@users.noreply.github.com>
2026-04-15 12:39:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello from Node.js CI/CD </h1>');
+    res.end('<h1>Webhook Trigger Test 🚀🔥</h1>');
 });
 
 server.listen(PORT, () => {
